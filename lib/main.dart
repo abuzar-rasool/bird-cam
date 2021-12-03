f141549adb4a8bee09ec912b1871775d49e9ea86
abuzarrasool <abuzar@oraan.com>
2021-12-03 15:02:14 +0500

    req

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,17 @@ import 'dart:io';
 import 'package:bird_cam/loading_animation.dart';
 import 'package:camera_camera/camera_camera.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:bird_cam/bird.dart';
 import 'package:bird_cam/database.dart';
 import 'package:loading_indicator/loading_indicator.dart';
+import 'package:http/http.dart' as http;
+
+String url = "http://cc38-34-91-67-109.ngrok.io/classify";
 
 void main() {
-  runApp( BirdCam());
+  runApp(BirdCam());
 }
 
 class BirdCam extends StatelessWidget {
@@ -20,11 +24,9 @@ class BirdCam extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'BirdCam',
-      theme: ThemeData(
-        primarySwatch: Colors.brown,
-        fontFamily: "SF-Pro-Display"
-      ),
-      home: Scaffold(body:  HomeCamera()),
+      theme:
+          ThemeData(primarySwatch: Colors.brown, fontFamily: "SF-Pro-Display"),
+      home: Scaffold(body: HomeCamera()),
     );
   }
 }
@@ -39,90 +41,108 @@ class HomeCamera extends StatefulWidget {
 class _HomeCameraState extends State<HomeCamera> {
   @override
   Widget build(BuildContext context) {
-    return CameraCamera(
-        onFile: (file) {
-          showBarModalBottomSheet(
-            context: context,
-            builder: (context) =>  Results(),
-          );
-        }
-);
+    return CameraCamera(onFile: (file) {
+      showBarModalBottomSheet(
+        context: context,
+        builder: (context) => Results(
+          file: file,
+        ),
+      );
+    });
   }
 }
 
 class Results extends StatelessWidget {
-  // final Bird bird;
-  Results({Key? key}) : super(key: key);
-  Bird b = birds[0];
-  bool loading = true;
+  Results({Key? key, required this.file}) : super(key: key);
+  Bird? b;
+  File file;
   final List<Map<String, dynamic>> _items = List.generate(
       5,
       (index) =>
           {"id": index, "title": "Item $index", "subtitle": "Subtitle $index"});
 
-  final List<String> titles = ["Habitat", "Diet", "Lifespan", "Family", "Interesting Fact"];
-  final List<IconData> leading_icons = [Icons.home_rounded, Icons.food_bank, Icons.health_and_safety, Icons.groups, Icons.favorite];
+  final List<String> titles = [
+    "Habitat",
+    "Diet",
+    "Lifespan",
+    "Family",
+    "Interesting Fact"
+  ];
+  final List<IconData> leadingIcons = [
+    Icons.home_rounded,
+    Icons.food_bank,
+    Icons.health_and_safety,
+    Icons.groups,
+    Icons.favorite
+  ];
 
-  Widget loadingWidget () {
+  Widget loadingWidget() {
     return Center(
-      child: Container(
+      child: SizedBox(
         // height: 300,
         width: 200,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          // ignore: prefer_const_literals_to_create_immutables
           children: [
-            LoadingIndicator(
-              indicatorType: Indicator.orbit, /// Required, The loading type of the widget
+            const LoadingIndicator(
+              indicatorType: Indicator.orbit,
+
+              /// Required, The loading type of the widget
             ),
-            SizedBox(height: 10,),
-            Text("Please wait. Running our CNN model...", style: TextStyle(fontSize: 14))
+            const SizedBox(
+              height: 10,
+            ),
+            const Text("Please wait. Running our CNN model...",
+                style: TextStyle(fontSize: 14))
           ],
         ),
       ),
     );
   }
 
-  Widget result_content (double screenHeight, List<String> b_details) {
-      return Column(
+  Widget resultContent(double screenHeight, List<String?> bDetails) {
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children:  [
+      children: [
         Container(
           alignment: Alignment.bottomLeft,
-          decoration:  BoxDecoration(
-            color: Colors.brown,
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(15),
-              bottomRight: Radius.circular(15),
-            ),
-            image: DecorationImage(
-              colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.4), BlendMode.dstATop),
-              fit: BoxFit.cover,
-              image:  AssetImage("assets/common_mynah.jpg",
-          ),
-            )
-          ),
+          decoration: BoxDecoration(
+              color: Colors.brown,
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(15),
+                bottomRight: Radius.circular(15),
+              ),
+              image: DecorationImage(
+                colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.4), BlendMode.dstATop),
+                fit: BoxFit.cover,
+                image: const AssetImage(
+                  "assets/common_mynah.jpg",
+                ),
+              )),
           height: screenHeight * 0.3,
           width: double.infinity,
-          child:  Padding(
-            padding: EdgeInsets.all(20.0),
-            child:  Column(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(b.name,
-                  style: TextStyle(
-                    color: Colors.white, 
+                Text(
+                  b?.name ?? "",
+                  style: const TextStyle(
+                    color: Colors.white,
                     fontSize: 32,
                     fontWeight: FontWeight.w500,
-                    ),
+                  ),
                 ),
-                Text(b.scientificName,
-                  style: TextStyle(
-                    color: Colors.white, 
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500
-                    )
-                ),
+                Text(b?.scientificName ?? "",
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    )),
               ],
             ),
           ),
@@ -131,19 +151,23 @@ class Results extends StatelessWidget {
         //   mainAxisAlignment: MainAxisAlignment.start,
         //   crossAxisAlignment: CrossAxisAlignment.start,
         //   children: [
-        Padding(
+        const Padding(
           padding: EdgeInsets.symmetric(horizontal: 1.0, vertical: 10.0),
-          child: Text("Details", style: TextStyle(fontSize: 28, fontWeight: FontWeight.w400, color: Colors.black54)),
+          child: Text("Details",
+              style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black54)),
         ),
         //       SizedBox(height: 15),
-          Expanded(
+        Expanded(
             child: ListTileTheme(
-          contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          contentPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
           iconColor: Colors.red,
           textColor: Colors.brown.shade800,
           tileColor: Colors.white,
-          shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15)),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
           style: ListTileStyle.list,
           dense: true,
           child: ListView.builder(
@@ -151,57 +175,81 @@ class Results extends StatelessWidget {
             itemBuilder: (_, index) => Card(
               color: Colors.white,
               elevation: 1.0,
-              margin: EdgeInsets.symmetric(horizontal: 5.0, vertical:5.0),
+              margin: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
               child: ListTile(
                 leading: CircleAvatar(
                   backgroundColor: Colors.brown.shade700,
                   child: IconButton(
-                          icon: Icon(leading_icons[index], color: Colors.white),
-                          onPressed: () {},
-                        ),
+                    icon: Icon(leadingIcons[index], color: Colors.white),
+                    onPressed: () {},
+                  ),
                 ),
-                title: Text(titles[index], 
-                  style: TextStyle(fontSize: 20),
+                title: Text(
+                  titles[index],
+                  style: const TextStyle(fontSize: 20),
                 ),
-                subtitle: Text(b_details[index],
-                style: TextStyle(fontSize: 16)),
+                subtitle:
+                    Text(bDetails[index] ?? "", style: TextStyle(fontSize: 16)),
               ),
             ),
           ),
-          )
-          )  
-          ],
-        );
-    }
+        ))
+      ],
+    );
+  }
 
-    Future getData() async {
-      await Future.delayed(Duration(seconds: 5));
-      return 1;
-    }
+
+  Future<Map<String,dynamic>?> getData(File file) async {
+    Response _response;
+    Map<String,dynamic>? _result;
+    print('uploading data');
+      var uri = Uri.parse(url);
+      var request = http.MultipartRequest("POST", uri);
+      request.files.add(await http.MultipartFile.fromPath('file', file.path));
+      try{
+      var res = await request.send();
+      _result= jsonDecode(await res.stream.bytesToString());
+      print(_result);
+      }catch(e){
+        print(e);
+        return null;
+      }
+      return _result;
+    //   return response.data['result'] as int;
+    // } catch (e) {
+    //   print(e);
+    //   return -3;
+    // }
+  }
 
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
-    final List<String> b_details = [b.habitat, b.diet, b.lifespan, b.lifespan, b.other];
-
+    final List<String?> bDetails = [
+      b?.habitat,
+      b?.diet,
+      b?.lifespan,
+      b?.lifespan,
+      b?.other
+    ];
     return FutureBuilder(
-    future: getData(), // the function to get your data from firebase or firestore
-    builder : (BuildContext context, AsyncSnapshot snap) {
-        if(!snap.hasData){
-            print("loading");
+        future: getData(
+            file), // the function to get your data from firebase or firestore
+        builder: (BuildContext context, AsyncSnapshot snap) {
+          if (!snap.hasData) {
             return loadingWidget();
-        } 
-        else {
-          print(snap.data);
-          return result_content(screenHeight, b_details);
-        }        
-    }
-);
-    
+          } else {
+            int result =int.parse( snap.data["result"] as String);
+            if (result <= 0) {
+              return const Center(
+                child: Text("No bird found", style: TextStyle(fontSize: 20)),
+              );
+            } else {
+              b = birds.firstWhere((element) => element.id == result);
+              return resultContent(screenHeight, bDetails);
+            }
+          }
+        });
   }
 }
-
-
-
-
